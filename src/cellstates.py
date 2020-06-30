410532fc9ca4963fbd850ba49c53265534a81046
hansinahuja <hansinahuja@gmail.com>
2020-07-01 00:29:15 +0530

    Fix cell dependency

--- src/cellstates.py
+++
@@ -1,5 +0,0 @@
-class Cell:
-    def __init__(self, parent, inQueue, isWall):
-        self.parent = parent 
-        self.inQueue = inQueue        # bool to check if already pushed in queue or not
-        self.isWall = isWall          # bool to check if wall              
--- src/environment.py
+++ src/environment.py
@@ -1,8 +1,15 @@
 from utilities import Coordinate
-from cellstates import Free
+
+class Cell:
+    def __init__(self, coordinate, parent = None, isWall = False, inQueue = False, isTraversed = False):
+        self.coordinate = coordinate
+        self.parent = parent 
+        self.isWall = isWall            # bool to check if wall  
+        self.inQueue = inQueue          # bool to check if already pushed in queue or not
+        self.isTraversed = isTraversed
 
 class Environment:
     def __init__(self, length, breadth):
         self.length = length
         self.breadth = breadth
-        self.grid = [ [Free(Coordinate(x, y)) for x in range(length)] for y in range(breadth) ]
+        self.grid = [ [Cell(Coordinate(x, y)) for x in range(breadth)] for y in range(length) ]
