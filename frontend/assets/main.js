efa55a32006bd1748080bb3983d3de0e7876cdd5
Ekansh Mahendru <ekanshmahendru@gmail.com>
2020-07-08 20:58:09 +0530

    Add code to gather data from the maze

--- frontend/assets/main.js
+++ frontend/assets/main.js
@@ -5,33 +5,33 @@ var box = [];
 var chid = 0;
 var margin = 0;
 
-function showNav(){
+function showNav() {
     nav = document.getElementsByTagName("nav")[0];
     nav.style.marginLeft = "0px";
 }
 
-function hideNav(){
+function hideNav() {
     nav = document.getElementsByTagName("nav")[0];
     nav.style.marginLeft = "-300px";
 }
 
 
-function makeGrid(){
+function makeGrid() {
     box = [];
     grid = document.getElementById("grid");
     grid.innerHTML = "";
-    n = Math.floor((window.innerWidth-margin)/40);
-    m = Math.floor(window.innerHeight/40);
-    for(var j=0; j<=m; j++){
+    n = Math.floor((window.innerWidth - margin) / 40);
+    m = Math.floor(window.innerHeight / 40);
+    for (var j = 0; j <= m; j++) {
         temp = []
-        for(var i=0; i<=n; i++){
+        for (var i = 0; i <= n; i++) {
             cell = document.createElement("div");
             cell.classList = ["cell"];
-            cell.id = j+"x"+i;
+            cell.id = j + "x" + i;
             drawingElement(cell);
             cell.oncontextmenu = addCheckpoint(cell.id);
-            cell.style.left = 40*i+"px";
-            cell.style.top = 40*j+"px";
+            cell.style.left = 40 * i + "px";
+            cell.style.top = 40 * j + "px";
             grid.appendChild(cell);
             temp.push(0);
         }
@@ -40,40 +40,40 @@ function makeGrid(){
     start = document.getElementById("start");
     stop = document.getElementById("stop");
 
-    box[Math.floor(m/2)][Math.floor(n/4)]=1;
-    document.getElementById(Math.floor(m/2)+"x"+Math.floor(n/4)).classList = "start cell";
-    start.style.left = margin+40*Math.floor(n/4)+20+"px";
-    start.style.top = 40*Math.floor(m/2)+20+"px";
-    start.style.zIndex = Math.floor(m/2);
+    box[Math.floor(m / 2)][Math.floor(n / 4)] = 1;
+    document.getElementById(Math.floor(m / 2) + "x" + Math.floor(n / 4)).classList = "start cell";
+    start.style.left = margin + 40 * Math.floor(n / 4) + 20 + "px";
+    start.style.top = 40 * Math.floor(m / 2) + 20 + "px";
+    start.style.zIndex = Math.floor(m / 2);
 
-    box[Math.floor(m/2)][Math.floor(3*n/4)]=1;
-    document.getElementById(Math.floor(m/2)+"x"+Math.floor(3*n/4)).classList = "stop cell";
-    stop.style.left = margin+40*Math.floor(3*n/4)+20+"px";
-    stop.style.top = 40*Math.floor(m/2)+20+"px";
-    stop.style.zIndex = Math.floor(m/2);
+    box[Math.floor(m / 2)][Math.floor(3 * n / 4)] = 1;
+    document.getElementById(Math.floor(m / 2) + "x" + Math.floor(3 * n / 4)).classList = "stop cell";
+    stop.style.left = margin + 40 * Math.floor(3 * n / 4) + 20 + "px";
+    stop.style.top = 40 * Math.floor(m / 2) + 20 + "px";
+    stop.style.zIndex = Math.floor(m / 2);
 }
 
 makeGrid();
 
 
-function addCheckpoint(id){
-    function addCheckpoint(){
+function addCheckpoint(id) {
+    function addCheckpoint() {
         i = id.split("x");
-        if(box[Number(i[0])][Number(i[1])]){
+        if (box[Number(i[0])][Number(i[1])]) {
             return false;
         }
-        if(chid>4){
+        if (chid > 4) {
             return false;
         }
         ch = document.createElement("img");
-        ch.src = "images/yellow"+(chid+1)+".svg";
+        ch.src = "images/yellow" + (chid + 1) + ".svg";
         ch.classList = "draggable checkpoint";
-        ch.id = "checkpoint"+chid;
+        ch.id = "checkpoint" + chid;
         ch.oncontextmenu = removeCheckpoint(chid);
         chid++;
         ch.style.zIndex = Number(i[0]);
-        ch.style.top = 40*Number(i[0])+20+"px";
-        ch.style.left = margin+40*Number(i[1])+20+"px";
+        ch.style.top = 40 * Number(i[0]) + 20 + "px";
+        ch.style.left = margin + 40 * Number(i[1]) + 20 + "px";
         dragElement(ch);
         box[Number(i[0])][Number(i[1])] = 1;
         document.body.appendChild(ch);
@@ -82,24 +82,24 @@ function addCheckpoint(id){
     return addCheckpoint
 }
 
-function removeCheckpoint(id){
-    function remove(e){
+function removeCheckpoint(id) {
+    function remove(e) {
         e.preventDefault();
-        ch = document.getElementById("checkpoint"+id);
+        ch = document.getElementById("checkpoint" + id);
         i = ch.style.left;
-        i = Number(i.substr(0,i.length-2));
-        i = Math.floor((i-margin)/40);
+        i = Number(i.substr(0, i.length - 2));
+        i = Math.floor((i - margin) / 40);
         j = ch.style.top;
-        j = Number(j.substr(0,j.length-2));
-        j = Math.floor(j/40);
+        j = Number(j.substr(0, j.length - 2));
+        j = Math.floor(j / 40);
         box[j][i] = 0;
         ch.parentNode.removeChild(ch);
         chid--;
-        for(var i = id+1; i<chid+1; i++){
-            ch = document.getElementById("checkpoint"+i);
-            ch.id = "checkpoint"+(i-1);
-            ch.oncontextmenu = removeCheckpoint(i-1);
-            ch.src = "images/yellow"+i+".svg";
+        for (var i = id + 1; i < chid + 1; i++) {
+            ch = document.getElementById("checkpoint" + i);
+            ch.id = "checkpoint" + (i - 1);
+            ch.oncontextmenu = removeCheckpoint(i - 1);
+            ch.src = "images/yellow" + i + ".svg";
         }
         return false;
     }
@@ -107,99 +107,108 @@ function removeCheckpoint(id){
 }
 
 function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, sx = 0, sy = 0, classes = "";
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0,
+        sx = 0,
+        sy = 0,
+        classes = "";
     elmnt.onmousedown = dragMouseDown;
-  
+
     function dragMouseDown(e) {
-      e = e || window.event;
-      if(e.button!=0) return false;
-      e.preventDefault();
-      elmnt.style.transform = "scale(1.2) translate(-35%,-70%)";
-      elmnt.style.cursor = "grabbing";
-      elmnt.style.zIndex = "1000";
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      sx = elmnt.style.left;
-      sy = elmnt.style.top;
-      box[Math.floor(Number(sy.substr(0,sy.length-2))/40)][Math.floor((Number(sx.substr(0,sx.length-2))-margin)/40)] = 0;
-      thiscell = document.getElementById(Math.floor(Number(sy.substr(0,sy.length-2))/40)+"x"+Math.floor((Number(sx.substr(0,sx.length-2))-margin)/40))
-      classes = thiscell.classList.value;
-      thiscell.classList = "cell";
-      document.onmouseup = closeDragElement;
-      document.onmousemove = elementDrag;
+        e = e || window.event;
+        if (e.button != 0) return false;
+        e.preventDefault();
+        elmnt.style.transform = "scale(1.2) translate(-35%,-70%)";
+        elmnt.style.cursor = "grabbing";
+        elmnt.style.zIndex = "1000";
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        sx = elmnt.style.left;
+        sy = elmnt.style.top;
+        box[Math.floor(Number(sy.substr(0, sy.length - 2)) / 40)][Math.floor((Number(sx.substr(0, sx.length - 2)) - margin) / 40)] = 0;
+        thiscell = document.getElementById(Math.floor(Number(sy.substr(0, sy.length - 2)) / 40) + "x" + Math.floor((Number(sx.substr(0, sx.length - 2)) - margin) / 40))
+        classes = thiscell.classList.value;
+        thiscell.classList = "cell";
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
     }
-  
+
     function elementDrag(e) {
-      e = e || window.event;
-      e.preventDefault();
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
-  
+
     function closeDragElement() {
         document.onmouseup = null;
         document.onmousemove = null;
-        t = Number(elmnt.style.top.substr(0,elmnt.style.top.length-2));
-        l = Number(elmnt.style.left.substr(0,elmnt.style.left.length-2));
-        if(box[Math.floor(t/40)][Math.floor((l-margin)/40)]!=0){
+        t = Number(elmnt.style.top.substr(0, elmnt.style.top.length - 2));
+        l = Number(elmnt.style.left.substr(0, elmnt.style.left.length - 2));
+        if (box[Math.floor(t / 40)][Math.floor((l - margin) / 40)] != 0) {
             elmnt.style.top = sy;
             elmnt.style.left = sx;
-            box[Math.floor(Number(sy.substr(0,sy.length-2))/40)][Math.floor((Number(sx.substr(0,sx.length-2))-margin)/40)] = 1;
-            document.getElementById(Math.floor(Number(sy.substr(0,sy.length-2))/40)+"x"+Math.floor((Number(sx.substr(0,sx.length-2))-margin)/40)).classList = classes;
-            elmnt.style.zIndex = Math.floor(Number(sy.substr(0,sy.length-2))/40);
-        }else{
-            elmnt.style.top = 40*Math.floor(t/40)+20+"px";
-            elmnt.style.left = Math.max(margin + 40*Math.floor((l-margin)/40)+20, margin+20)+"px";
-            box[Math.floor(t/40)][Math.floor((l-margin)/40)] = 1;
-            document.getElementById(Math.floor(t/40)+"x"+Math.floor((l-margin)/40)).classList = classes;
-            elmnt.style.zIndex = Math.floor(t/40);
+            box[Math.floor(Number(sy.substr(0, sy.length - 2)) / 40)][Math.floor((Number(sx.substr(0, sx.length - 2)) - margin) / 40)] = 1;
+            document.getElementById(Math.floor(Number(sy.substr(0, sy.length - 2)) / 40) + "x" + Math.floor((Number(sx.substr(0, sx.length - 2)) - margin) / 40)).classList = classes;
+            elmnt.style.zIndex = Math.floor(Number(sy.substr(0, sy.length - 2)) / 40);
+        } else {
+            elmnt.style.top = 40 * Math.floor(t / 40) + 20 + "px";
+            elmnt.style.left = Math.max(margin + 40 * Math.floor((l - margin) / 40) + 20, margin + 20) + "px";
+            box[Math.floor(t / 40)][Math.floor((l - margin) / 40)] = 1;
+            document.getElementById(Math.floor(t / 40) + "x" + Math.floor((l - margin) / 40)).classList = classes;
+            elmnt.style.zIndex = Math.floor(t / 40);
         }
         elmnt.style.transform = "scale(1) translate(-35%,-70%)";
         elmnt.style.cursor = "grab";
     }
 }
 
-for(let elmnt of document.getElementsByClassName("draggable")){
+for (let elmnt of document.getElementsByClassName("draggable")) {
     dragElement(elmnt);
 }
 
 function drawingElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
     elmnt.onmousedown = dragMouseDown;
-  
+
     function dragMouseDown(e) {
         e = e || window.event;
         e.preventDefault();
-        if(e.button!=0) return false;
+        if (e.button != 0) return false;
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
         i = elmnt.id.split('x');
-        if(box[Number(i[0])][Number(i[1])]==2){
+        if (box[Number(i[0])][Number(i[1])] == 2) {
             box[Number(i[0])][Number(i[1])] = 0;
             elmnt.classList = "cell";
             document.onmousemove = elementDragOFF;
-        }else if(box[Number(i[0])][Number(i[1])]==0){
+        } else if (box[Number(i[0])][Number(i[1])] == 0) {
             box[Number(i[0])][Number(i[1])] = 2;
             elmnt.classList = "wall cell";
             document.onmousemove = elementDragON;
         }
     }
-    
+
     function elementDragON(e) {
         e = e || window.event;
         e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
-        i = Math.floor((pos3-margin)/40);
-        j = Math.floor(pos4/40);
-        if(box[j][i]!=1){
+        i = Math.floor((pos3 - margin) / 40);
+        j = Math.floor(pos4 / 40);
+        if (box[j][i] != 1) {
             box[j][i] = 2;
-            document.getElementById(j+'x'+i).classList = "wall cell"
+            document.getElementById(j + 'x' + i).classList = "wall cell"
         }
     }
 
@@ -208,11 +217,11 @@ function drawingElement(elmnt) {
         e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
-        i = Math.floor((pos3-margin)/40);
-        j = Math.floor(pos4/40);
-        if(box[j][i]!=1){
+        i = Math.floor((pos3 - margin) / 40);
+        j = Math.floor(pos4 / 40);
+        if (box[j][i] != 1) {
             box[j][i] = 0;
-            document.getElementById(j+'x'+i).classList = "cell";
+            document.getElementById(j + 'x' + i).classList = "cell";
         }
     }
 
