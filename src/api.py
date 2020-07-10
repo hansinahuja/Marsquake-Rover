c91f897efbf6819ae63aa8b2186220bfaa8af80d
Ekansh Mahendru <ekanshmahendru@gmail.com>
2020-07-10 09:44:37 +0530

    Host static files on the flask server

--- src/api.py
+++ src/api.py
@@ -2,13 +2,26 @@ import os
 import re
 import flask
 import json
-from flask import request, jsonify
+from flask import request, jsonify, send_from_directory, render_template
 from flask_cors import CORS
 from driver_2 import driver
 
-app = flask.Flask(__name__)
+template_dir = os.path.abspath('../frontend')
+app = flask.Flask(__name__, template_folder=template_dir)
 CORS(app)
 app.config["DEBUG"] = False
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/assets/<path:path>')
+def assets(path):
+    return send_from_directory('../frontend/assets', path)
+
+@app.route('/images/<path:path>')
+def images(path):
+    return send_from_directory('../frontend/images', path)
 
 @app.route('/api/findpath/', methods=['POST'])
 def findpath():
