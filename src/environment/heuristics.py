dfe5fa894e355db07a654df28885396d0e11da1d
hansinahuja <hansinahuja@gmail.com>
2020-07-21 13:43:52 +0530

    Comments

--- src/environment/heuristics.py
+++ src/environment/heuristics.py
@@ -3,6 +3,16 @@ from environment.utils import Location
 
 def distance(self, src, dest):
 
+    """
+    Returns cost of travelling from one cell to another.
+    Args:
+        src: Source cell.
+        dest: Destination cell.
+    Returns:
+        Cost of travelling from src to dest.
+    """
+
+    # Cost of taking a wormhole = 0
     if src.type == 'wormholeEntry' and dest.type=='wormholeExit':
         return 0
 
@@ -11,15 +21,36 @@ def distance(self, src, dest):
     return dest.weight * sqrt(manhattanDistance(deltaX, deltaY))
 
 def bestHeuristic(self, source, destinations):
+
+    """
+    Returns best heuristic from source to any destination.
+    Args:
+        source: Source cell.
+        destinations: List of destination cells.
+    Returns:
+        minHeuristic: Best source to destination heuristic.
+    """
+
     minHeuristic = self.length + self.breadth
     for destination in destinations:
         minHeuristic = min(minHeuristic, heuristic(source, destination, self.heuristic))
     return minHeuristic
 
 
-def heuristic(cellA, cellB, heuristic):
-    deltaX = abs(cellA.location.x - cellB.location.x)
-    deltaY = abs(cellA.location.y - cellB.location.y)
+def heuristic(src, dest, heuristic):
+
+    """
+    Returns heuristic from source to destination.
+    Args:
+        src: Source cell.
+        dest: Destination cell.
+        heuristic: Heuristic choice.
+    Returns:
+        Distance between src and dest based on selected heuristic.
+    """
+
+    deltaX = abs(src.location.x - dest.location.x)
+    deltaY = abs(src.location.y - dest.location.y)
     if heuristic == 'manhattan':
         return manhattanDistance(deltaX, deltaY)
     elif heuristic == 'euclidean':
@@ -30,6 +61,7 @@ def heuristic(cellA, cellB, heuristic):
         return chebyshevDistance(deltaX, deltaY)
 
 
+# Helper functions to calculate heuristics
 def manhattanDistance(deltaX, deltaY):
     return deltaX + deltaY
 
--- src/modes/checkpointMode.py
+++ src/modes/checkpointMode.py
@@ -1,39 +1,50 @@
 from modes.nonCheckpointMode import nonCheckpointMode
 
+def checkpointMode(config):
 
-def checkpointMode(dict):
-    # print("IN")
-    # print(dict)
-    # print("===================================================")
+    """
+    Simulates path-finding in checkpoint mode.
+    Args:
+        config: Dictionary with all the configuration settings.
+    Returns:
+        output: Final path taken and list of changes on the grid.
+    """
+
+    # Extract points to be visited in order.
     points = []
-    for point in dict['start']:
+    for point in config['start']:
         points.append(point)
-    for point in dict['checkpoints']:
+    for point in config['checkpoints']:
         points.append(point)
-    for point in dict['stop']:
+    for point in config['stop']:
         points.append(point)
 
     gridChanges = []
     path = []
-    # dict['biDirectional'] = '0'
-    dict['checkpoints'] = []
+    config['checkpoints'] = []
+
+    # Iterate over all checkpoints
     for i in range(len(points)-1):
-        # print(i)
-        dict['start'] = [points[i]]
-        dict['stop'] = [points[i+1]]
-        result = nonCheckpointMode(dict)
+
+        # Treat each checkpoint as a destination in non-checkpoint mode
+        config['start'] = [points[i]]
+        config['stop'] = [points[i+1]]
+        result = nonCheckpointMode(config)
         gridChanges.extend(result['gridChanges'])
+
+        # Break if no path is found
         if len(result['path']) == 0:
             break
         else:
             path.extend(result['path'])
-            if(len(points)>2):
+
+            # Clear the grid only if checkpoints present
+            if(len(points) > 2):
                 gridChanges.extend(result['activatedCells'])
+
+            # To remove repeated points in path
             if i < len(points)-2:
                 path.pop()
-    # gridChanges = []
+
     output = {'gridChanges': gridChanges, 'path': path}
-    # print(path)
-    # print("===================")
-    # print(gridChanges)
     return output
